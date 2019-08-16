dea8cf1aab82d4d6f962c6e83090da6f98a305e7
Guilherme Fernandes De Sá Villela <20181ENEL0282@cefetes.br>
2019-08-16 17:40:54 -0300

    fazendo ainda

--- Acomp2/SelectionSort.cs
+++ Acomp2/SelectionSort.cs
@@ -3,8 +3,26 @@
     class SelectionSort : ISort
     {
         public void Sort(int[] E, int n)
-        {
-            throw new System.NotImplementedException();
+        {         
+            for (int i = 0; i < n - 1; i++)
+            {
+                int k = i;
+
+                int x = E[i];
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (E[j] < x)
+                    {
+                        k = j;
+                        x = E[k];
+                    }
+                }
+
+                E[k] = E[i];
+                E[i] = x;
+
+            }
         }
     }
 }
